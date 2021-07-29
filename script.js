6460d68e5890623598b246a456420bba68caea05
mytrofanov <mytrofanovmaxym@gmail.com>
2021-07-29 11:05:56 +0300

    Задача вывести массив от 1 до 7

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // document.querySelector('button').onclick = palindrom;  // устаревший код
-document.querySelector('.b-1').addEventListener('click', palindrom)
+// document.querySelector('.b-1').addEventListener('click', palindrom)
+// document.querySelector('.b-2').addEventListener('click', array)
 
+
+// Привязка функций к кнопкам :
+let but = (sel, fun) => {
+    document.querySelector(sel).addEventListener('click', fun)
+}
+but('.b-1', palindrom);
+but('.b-2', arr);
+but('.b-3', clean);
+
+// Палиндром
 function palindrom() {
     let word = document.querySelector('.i-1').value
     document.querySelector('.out_word').innerHTML = word.toLowerCase()
@@ -11,4 +22,13 @@ function palindrom() {
     } else (
         document.querySelector('.conclusion').innerHTML = 'слова не равны'
     )
+}
+// Массив от 1 до 7
+function arr() {
+    for (let i=1; i<8; i++) {
+        document.querySelector('.array_numbers').innerHTML += i+'<br>'
+    }
+}
+function clean () {
+    document.querySelector('.array_numbers').innerHTML = null;
 }
