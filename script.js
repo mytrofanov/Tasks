d2545517a1d9789b457934433050ae87fd7acefe
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-10 05:56:57 +0200

    разбить число на массив чисел, сортировка, склеивание чисел в одно число

--- script.js
+++ script.js
@@ -708,3 +708,19 @@ function TheBiggest() {
 // }
 //
 // console.log(result)
+//Your task is to make a function that can take any non-negative integer as an argument
+// and return it with its digits in descending order. Essentially,
+// rearrange the digits to create the highest possible number.
+//Examples:
+ //   Input: 42145 Output: 54421
+//Input: 145263 Output: 654321
+//Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+    const arrayOfDigits = Array.from(String(n), Number);
+    arrayOfDigits.sort(function (a,b) {return b - a
+    })
+    let result = +arrayOfDigits.join('')
+    return result
+}
+console.log(descendingOrder(145263))
