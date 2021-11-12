1248c5d47c45880922b70448127a078d48cabf32
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-12 10:17:28 +0200

    Решение задачи про очередь в кассе

--- script.js
+++ script.js
@@ -777,34 +777,83 @@ function TheBiggest() {
 //
 // Note:
 //     If you are given an array with multiple answers, return the lowest correct index.
+//
+// arr = [20,10,30,10,10,15,35]
+//
+// function findEvenIndex(arr) {
+//     if (!arr.length||arr.length>1000) {
+//         return null
+//     }
+//     let left = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         let right = 0
+//         if (i>0) {
+//             left += arr[i-1]
+//         }
+//             right = arr.slice(i+1,arr.length)
+//         let res
+//             if (right.length>0) {
+//                 res = right.reduce(function (sum,elem) {
+//                     return sum +elem
+//                 })
+//             }
+//
+//             console.log(`i=${i} left= ${left}; right= ${res}`)
+//             if (left === res) {
+//                 return i
+//             }
+//     }
+//     return -1
+// }
+//
+// console.log(findEvenIndex(arr))
 
-arr = [20,10,30,10,10,15,35]
+//==============================================================================
+// There is a queue for the self-checkout tills at the supermarket. Your task is write
+// a function to calculate the total time required for all the customers to check out!
+//
+//     input
+// customers: an array of positive integers representing the queue. Each integer represents
+// a customer, and its value is the amount of time they require to check out.
+//     n: a positive integer, the number of checkout tills.
+//     output
+// The function should return an integer, the total time required.
+//
+//     Important
+// Please look at the examples and clarifications below, to ensure you understand the task correctly :)
+//
+// Examples
+// queueTime([5,3,4], 1)
+// // should return 12
+// // because when there is 1 till, the total time is just the sum of the times
+//
+// queueTime([10,2,3,3], 2)
+// // should return 10
+// // because here n=2 and the 2nd, 3rd, and 4th people in the
+// // queue finish before the 1st person has finished.
+//
+// queueTime([2,3,10], 2)
+// // should return 12
+// Clarifications
+// There is only ONE queue serving many tills, and
+// The order of the queue NEVER changes, and
+// The front person in the queue (i.e. the first element in the array/list) proceeds
+// to a till as soon as it becomes free.
+//     N.B. You should assume that all the test input will be valid, as specified above.
+//
+//     P.S. The situation in this kata can be likened to the more-computer-science-related idea
+//     of a thread pool, with relation to running multiple processes
+//     at the same time: https://en.wikipedia.org/wiki/Thread_pool
 
-function findEvenIndex(arr) {
-    if (!arr.length||arr.length>1000) {
-        return null
+function queueTime(customers, tillsNumber) {
+    let tills = new Array(tillsNumber).fill(0)
+    for (let t of customers) {
+        let index = tills.indexOf(Math.min(...tills))
+        tills[index] +=t
+        console.log('tills:')
+        console.log(tills)
     }
-    let left = 0
-    for (let i = 0; i < arr.length; i++) {
-        let right = 0
-        if (i>0) {
-            left += arr[i-1]
-        }
-            right = arr.slice(i+1,arr.length)
-        let res
-            if (right.length>0) {
-                res = right.reduce(function (sum,elem) {
-                    return sum +elem
-                })
-            }
 
-            console.log(`i=${i} left= ${left}; right= ${res}`)
-            if (left === res) {
-                return i
-            }
-    }
-    return -1
+    return Math.max(...tills)
 }
-
-console.log(findEvenIndex(arr))
-
+console.log(queueTime([2,3,10], 2))
