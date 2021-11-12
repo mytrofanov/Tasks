00c1cc906e9d8de254921f7a38cad9e95780f343
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-12 15:44:36 +0200

    число нарцисс если сумма всех элементов числа возведенного в степень равной количеству символов числа будет равно самому числу

--- script.js
+++ script.js
@@ -994,22 +994,54 @@ function TheBiggest() {
 // Have fun coding it and please don't forget to vote and rank this kata! :-)
 //
 // I have also created other katas. Take a look if you enjoyed this kata!
+//
+// function five(start, end) {
+//     let result = ''
+//     let count = 1
+//     for(let i=start; i<end; i++) {
+//         let str = String(i)
+//         if(!str.includes('5')){
+//             result+=i
+//             count +=1
+//             if(i<end-1){
+//                 result+=','
+//             }
+//         }
+//
+//     }
+//     console.log(result)
+//     console.log(count)
+// }
+// five(4,17)
 
-function five(start, end) {
-    let result = ''
-    let count = 1
-    for(let i=start; i<end; i++) {
-        let str = String(i)
-        if(!str.includes('5')){
-            result+=i
-            count +=1
-            if(i<end-1){
-                result+=','
-            }
-        }
+//================================================================================================
+// A Narcissistic Number is a positive number which is the sum of its own digits,
+//     each raised to the power of the number of digits in a given base.
+//     In this Kata, we will restrict ourselves to decimal (base 10).
+//
+// For example, take 153 (3 digits), which is narcisstic:
+//
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+//
+// 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+//
+//     Your code must return true or false (not 'true' and 'false')
+// depending upon whether the given number is a Narcissistic number in base 10.
+// This may be True and False in your language, e.g. PHP.
+//
+//     Error checking for text strings or other invalid inputs is not required,
+//     only valid positive non-zero integers will be passed into the function.
 
+function narcissistic(value) {
+    let sNum=String(value)
+    let sum = 0
+    for(let i =0; i<sNum.length; i++) {
+        let n = new Number(sNum[i])
+        sum += Math.pow(n,sNum.length)
     }
-    console.log(result)
-    console.log(count)
+    return sum === value
 }
-five(4,17)
+
+console.log(narcissistic(1652))
