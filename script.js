21d5567ac2de2ba3e8d70437e71156498b45939f
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-14 10:43:15 +0200

    сравниваю два массива, второй массив должен быть заполнен квадратами чисел первого

--- script.js
+++ script.js
@@ -1075,28 +1075,81 @@ function TheBiggest() {
 //     Examples:
 // toWeirdCase( "String" );//=> returns "StRiNg"
 // toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+//
+// let weird = "Weird string case"
+// function toWeirdCase(string) {
+//     let result = ''
+//     let space = string.split(' ')
+//
+//     for (let a=0; a<space.length; a++) {
+//
+//         for (let i=0; i<space[a].length; i++) {
+//             if (i % 2 === 0) {
+//                 result += space[a][i].toUpperCase()
+//             }
+//             if (i % 2 != 0) {
+//                 result += space[a][i].toLowerCase()
+//             }
+//         }
+//         if (space[a].length) {
+//             result +=' '
+//         }
+//     }
+//
+//     result = result.slice(0,result.length-1)
+//     return result
+// }
+// console.log(toWeirdCase( "Weird string case" ))
 
-let weird = "Weird string case"
-function toWeirdCase(string) {
-    let result = ''
-    let space = string.split(' ')
+//=========================================================================================
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b))
+// that checks whether the two arrays have the "same" elements, with the same multiplicities.
+// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+//
+//     Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the
+// square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on.
+// It gets obvious if we write b's elements in terms of squares:
+//
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp may not return true anymore:
+//
+//     a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+//
+//     a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+//
+//     Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, Crystal, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
 
-    for (let a=0; a<space.length; a++) {
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 
-        for (let i=0; i<space[a].length; i++) {
-            if (i % 2 === 0) {
-                result += space[a][i].toUpperCase()
+function validArray(array1, array2) {
+    if (array1.length === 0 || array2.length === 0 || array1.length != array2.length) {
+        return null
+    }
+    for (let i = 0; i < array1.length; i++) {
+        let square = array1[i] * array1[i]
+        for (let a = 0; a < array2.length; a++) {
+
+            if (array2[a] === square) {
+                 array2.splice(array2.indexOf(array2[a]),1)
+
+            if (array2.length === 0 ) {return  true}
             }
-            if (i % 2 != 0) {
-                result += space[a][i].toLowerCase()
-            }
-        }
-        if (space[a].length) {
-            result +=' '
         }
     }
-
-    result = result.slice(0,result.length-1)
-    return result
+    return false
 }
-console.log(toWeirdCase( "Weird string case" ))
+console.log (validArray(a,b))
