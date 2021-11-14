75d7cfa2c47ac984b666d67a22c079c95c04600c
mytrofanov <mytrofanovmaxym@gmail.com>
2021-11-14 10:13:26 +0200

    Каждую четную букву в слове делаем заглавной, в предложении каждое новое слово начинается с заглавной

--- script.js
+++ script.js
@@ -1033,15 +1033,70 @@ function TheBiggest() {
 //
 //     Error checking for text strings or other invalid inputs is not required,
 //     only valid positive non-zero integers will be passed into the function.
+//
+// function narcissistic(value) {
+//     let sNum=String(value)
+//     let sum = 0
+//     for(let i =0; i<sNum.length; i++) {
+//         let n = new Number(sNum[i])
+//         sum += Math.pow(n,sNum.length)
+//     }
+//     return sum === value
+// }
+//
+// console.log(narcissistic(1652))
 
-function narcissistic(value) {
-    let sNum=String(value)
-    let sum = 0
-    for(let i =0; i<sNum.length; i++) {
-        let n = new Number(sNum[i])
-        sum += Math.pow(n,sNum.length)
+//==============================================================================
+// Write a function toWeirdCase (weirdcase in Ruby) that accepts a string,
+//     and returns the same string with all even indexed characters in each word upper cased,
+//     and all odd indexed characters in each word lower cased. The indexing just explained
+// is zero based, so the zero-ith index is even, therefore that character should be upper cased
+// and you need to start over for each word.
+//
+//     The passed in string will only consist of alphabetical characters and spaces(' ').
+//     Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+//
+//     Examples:
+// toWeirdCase( "String" );//=> returns "StRiNg"
+// toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+
+//===============================================================================================
+
+// Write a function toWeirdCase (weirdcase in Ruby) that accepts a string,
+// and returns the same string with all even indexed characters in each word upper cased,
+// and all odd indexed characters in each word lower cased. The indexing just explained is zero based,
+// so the zero-ith index is even, therefore that character should be upper cased and you need
+// to start over for each word.
+//
+//     The passed in string will only consist of alphabetical characters and spaces(' ').
+//     Spaces will only be present if there are multiple words.
+//     Words will be separated by a single space(' ').
+//
+//     Examples:
+// toWeirdCase( "String" );//=> returns "StRiNg"
+// toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+
+let weird = "Weird string case"
+function toWeirdCase(string) {
+    let result = ''
+    let space = string.split(' ')
+
+    for (let a=0; a<space.length; a++) {
+
+        for (let i=0; i<space[a].length; i++) {
+            if (i % 2 === 0) {
+                result += space[a][i].toUpperCase()
+            }
+            if (i % 2 != 0) {
+                result += space[a][i].toLowerCase()
+            }
+        }
+        if (space[a].length) {
+            result +=' '
+        }
     }
-    return sum === value
-}
 
-console.log(narcissistic(1652))
+    result = result.slice(0,result.length-1)
+    return result
+}
+console.log(toWeirdCase( "Weird string case" ))
